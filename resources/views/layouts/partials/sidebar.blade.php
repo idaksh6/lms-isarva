d758c77adb11207df31fb50aa28820567a1126c9
idaksh <idaksh.6@idaksh.in>
2026-08-07 11:44:26 +0530

    sidebar update

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -57,7 +57,7 @@
                     <svg class="h-4 w-4 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
                     </svg>
-                    <span>Logout</span>
+                    <span>Logout Logout</span>
                 </button>
             </form>
         </div>
